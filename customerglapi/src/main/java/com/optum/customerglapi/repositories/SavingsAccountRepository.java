6c25037cc3f3979830dce3e81a3d842fa80dfb7b
eswaribala <parameswaribala@gmail.com>
2023-05-08 21:52:52 +0530

    updated

--- customerglapi/src/main/java/com/optum/customerglapi/repositories/SavingsAccountRepository.java
+++ customerglapi/src/main/java/com/optum/customerglapi/repositories/SavingsAccountRepository.java
@@ -2,6 +2,14 @@ package com.optum.customerglapi.repositories;
 
 import com.optum.customerglapi.domains.SavingsAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface SavingsAccountRepository extends JpaRepository<SavingsAccount, Long> {
+
+    @Query("Select s from SavingsAccount s where s.interestRate=:roi")
+    public List<SavingsAccount> findByROI(@Param("roi") float roi);
+
 }
