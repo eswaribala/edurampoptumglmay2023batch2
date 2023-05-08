86a162fdaf1c1f12dcfb59cf9cfdcaa1c222812a
eswaribala <parameswaribala@gmail.com>
2023-05-08 22:02:58 +0530

    updated

--- customerglapi/src/main/java/com/optum/customerglapi/repositories/CurrentAccountRepository.java
+++ customerglapi/src/main/java/com/optum/customerglapi/repositories/CurrentAccountRepository.java
@@ -1,7 +1,15 @@
 package com.optum.customerglapi.repositories;
 
 import com.optum.customerglapi.domains.CurrentAccount;
+import com.optum.customerglapi.domains.SavingsAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CurrentAccountRepository extends JpaRepository<CurrentAccount,Long> {
+
+    @Query("Select c from CurrentAccount c where c.overDraftLimit=:od")
+    public List<CurrentAccount> findByOD(@Param("od")  long od);
 }
