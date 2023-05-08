3d7baf63a72eb7b70fc339101be59c62942435bd
eswaribala <parameswaribala@gmail.com>
2023-05-08 22:59:03 +0530

    updated

--- customerglapi/src/main/java/com/optum/customerglapi/queries/CurrentAccountQuery.java
+++ customerglapi/src/main/java/com/optum/customerglapi/queries/CurrentAccountQuery.java
@@ -1,8 +1,26 @@
 package com.optum.customerglapi.queries;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.optum.customerglapi.domains.CurrentAccount;
+import com.optum.customerglapi.repositories.CurrentAccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class CurrentAccountQuery implements GraphQLQueryResolver {
+    @Autowired
+    private CurrentAccountRepository currentAccountRepository;
+
+    public List<CurrentAccount> findAllCurrentAccounts(){
+        return this.currentAccountRepository.findAll();
+    }
+    public CurrentAccount findCurrentAccountById(long accountNo){
+        return this.currentAccountRepository.findById(accountNo).orElse(null);
+    }
+    public List<CurrentAccount> findCurrentAccountByOD(long od){
+        return this.currentAccountRepository.findByOD(od);
+    }
+
 }
