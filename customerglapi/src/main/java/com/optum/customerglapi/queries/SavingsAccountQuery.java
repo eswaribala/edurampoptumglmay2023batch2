f7289e33bc557d5cae5592e4c4b902b9cd39c756
eswaribala <parameswaribala@gmail.com>
2023-05-08 22:55:43 +0530

    updated

--- customerglapi/src/main/java/com/optum/customerglapi/queries/SavingsAccountQuery.java
+++ customerglapi/src/main/java/com/optum/customerglapi/queries/SavingsAccountQuery.java
@@ -1,8 +1,28 @@
 package com.optum.customerglapi.queries;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.optum.customerglapi.domains.SavingsAccount;
+import com.optum.customerglapi.repositories.SavingsAccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class SavingsAccountQuery implements GraphQLQueryResolver {
+
+    @Autowired
+    private SavingsAccountRepository savingsAccountRepository;
+   public List<SavingsAccount> findAllSavingsAccount(){
+          return this.savingsAccountRepository.findAll();
+   }
+   public SavingsAccount findSavingsAccountById(long accountNo){
+           return this.savingsAccountRepository.findById(accountNo).orElse(null);
+   }
+    public List<SavingsAccount> findSavingsAccountByROI(float roi) {
+
+       return this.savingsAccountRepository.findByROI(roi);
+    }
+
+
 }
