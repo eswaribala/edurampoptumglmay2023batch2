a8dadd337f3276c73f0a51f6917cdcc8e98a2610
eswaribala <parameswaribala@gmail.com>
2023-05-08 20:29:53 +0530

    updated

---
+++ customerglapi/src/main/java/com/optum/customerglapi/CustomerglapiApplication.java
@@ -0,0 +1,13 @@
+package com.optum.customerglapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerglapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerglapiApplication.class, args);
+    }
+
+}
---
+++ customerglapi/src/test/java/com/optum/customerglapi/CustomerglapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optum.customerglapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerglapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
