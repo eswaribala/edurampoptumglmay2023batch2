d16aaadc7341017a0d3f7d78d9de895b07cf6dc2
eswaribala <parameswaribala@gmail.com>
2023-05-09 21:53:35 +0530

    updated

---
+++ transactionpublisher/src/main/java/com/optum/transactionpublisher/TransactionpublisherApplication.java
@@ -0,0 +1,13 @@
+package com.optum.transactionpublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionpublisherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionpublisherApplication.class, args);
+    }
+
+}
---
+++ transactionpublisher/src/test/java/com/optum/transactionpublisher/TransactionpublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optum.transactionpublisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionpublisherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
