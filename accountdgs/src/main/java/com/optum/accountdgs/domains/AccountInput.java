1d7ed818e44760cab01a9eef5acef8444f88394d
eswaribala <parameswaribala@gmail.com>
2023-05-10 22:47:30 +0530

    updated

---
+++ accountdgs/src/main/java/com/optum/accountdgs/domains/AccountInput.java
@@ -0,0 +1,17 @@
+package com.optum.accountdgs.domains;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccountInput {
+    private long accountNo;
+    private long runningTotal;
+    private String openingDate;
+    private List<TransactionInput> transactionInputList;
+}
---
+++ accountdgs/src/main/java/com/optum/accountdgs/domains/TransactionInput.java
@@ -0,0 +1,17 @@
+package com.optum.accountdgs.domains;
+
+import com.optum.accountdgs.domains.Account;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TransactionInput {
+
+    private long transactionId;
+    private long amount;
+    private String transactionDate;
+    private AccountInput account;
+}
