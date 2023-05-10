f374e60ed4a689833b410f713efcd156d1e5330d
eswaribala <parameswaribala@gmail.com>
2023-05-10 21:46:11 +0530

    updated

---
+++ accountdgs/src/main/java/com/optum/accountdgs/AccountdgsApplication.java
@@ -0,0 +1,13 @@
+package com.optum.accountdgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountdgsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountdgsApplication.class, args);
+    }
+
+}
---
+++ accountdgs/src/test/java/com/optum/accountdgs/AccountdgsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optum.accountdgs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountdgsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
