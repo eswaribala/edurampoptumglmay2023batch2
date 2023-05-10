826be8e029b49fae807c3f47cb64eb53e1d0df27
eswaribala <parameswaribala@gmail.com>
2023-05-10 19:53:27 +0530

    updated

---
+++ ticketdgs/src/main/java/com/optum/ticketdgs/TicketdgsApplication.java
@@ -0,0 +1,13 @@
+package com.optum.ticketdgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketdgsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketdgsApplication.class, args);
+    }
+
+}
---
+++ ticketdgs/src/test/java/com/optum/ticketdgs/TicketdgsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optum.ticketdgs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketdgsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
